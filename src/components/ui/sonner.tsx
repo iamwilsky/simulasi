6940d0297315a2edd28b5b0c7d0e0cf2a36bfc79
iamwilsky <willyarsal@gmail.com>
2026-03-11 14:02:28 +0700

    style: add close button and reduce duration to sonner toaster

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,6 +10,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      closeButton
+      duration={3000}
       toastOptions={{
         classNames: {
           toast:
